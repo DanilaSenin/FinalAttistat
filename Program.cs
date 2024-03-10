120e9423dcce39d12a1f1b36f8ccab6b575b4923
Daniil_Senin <16502711d.ru@gmail.com>
2024-03-10 21:22:04 +0300

    Main cicle

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿string [] Array = {"Rus", "rk", "Kazan"};
 string [] Result = new string[Array.Length];
+int h = Array.Length;
+int NumElem = 0;
+
+for (int i = 0; i<h; i+=1) {
+    if (Array[i].Length <= 3) {
+        Result[NumElem] = Array[i];
+        NumElem += 1;
+    }
+}
