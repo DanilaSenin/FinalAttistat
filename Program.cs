d1edef6dd8af081fe83ec6b5697387d7475b30ad
Daniil_Senin <16502711d.ru@gmail.com>
2024-03-10 22:02:10 +0300

    ReadMeFile

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿string [] Array = {"27", "354", "365", "-1", "354223"};
 string [] Result = new string[Array.Length];
 int h = Array.Length;
-int NumElem = 0;
+int NumElem = -1;
 
 for (int i = 0; i<h; i+=1) {
     if (Array[i].Length <= 3) {
-        Result[NumElem] = Array[i];
         NumElem += 1;
+        Result[NumElem] = Array[i];  
     }
 }
 
